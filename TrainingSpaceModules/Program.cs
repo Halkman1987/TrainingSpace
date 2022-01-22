87cff3df8b5fa494b88b1adb10ccb57e94973b32
Halkman <halk-man@mail.ru>
2022-01-22 23:17:47 +0500

    Задания модуля 9.6.1 и 9.6.2

--- TrainingSpaceModules/Program.cs
+++ TrainingSpaceModules/Program.cs
@@ -14,10 +14,26 @@ namespace FinalTask
 
             }
         }
+        public class SortList
+        {
+            public delegate void SortFamDel(int a);
+            public event SortFamDel SortFamEvent;
+            public void Read()
+            {
+                Console.WriteLine("Введите число 1 или 2");
+                int num = Convert.ToInt32(Console.ReadLine());
+                if (num != 1 && num != 2) throw new MyExciption();
+                NumberEnter(num);
 
+            }
+            public void NumberEnter(int a)
+            {
+                SortFamEvent?.Invoke(a);
+            }
+        }
         static void Main()
         {
-
+            // ----------------------------------------------- ПЕРВОЕ ЗАДАНИЕ С ВЫВОДОМ МАССИВА ИСКЛЮЧЕНИЙ ------------------------------------------------
             Exception[] exc = { new FormatException("Это сработало Формат Эксэпшн"), new ArgumentNullException(), new MyExciption(), new RankException("Опять этот РанкЭксэпшн"), new TimeoutException() };
             foreach (Exception ex in exc)
             {
@@ -45,6 +61,46 @@ namespace FinalTask
                 {
                     Console.WriteLine(ex4.GetType());
                     Console.WriteLine(ex4.Message);
+                }
+            }
+            // -------------------------------------------------- КОНЕЦ ПЕРВОГО ЗАДАНИЯ -------------------------------------------------------------------
+
+
+            // ------------------------------------------------- ВТОРОЕ ЗАДАНИЕ С СОРТИРОВКОЙ ФАМИЛИЙ -----------------------------------------------------
+            List<string> Family = new List<string>();
+            Family.Add("Гринн");
+            Family.Add("Огородников");
+            Family.Add("Арбузов");
+            Family.Add("Шварцнеггер");
+            Family.Add("Коломбо");
+            Family.Add("Прист");
+            Family.Add("Катллер");
+            SortList sortList = new SortList();
+            sortList.SortFamEvent += Sorted;
+            try
+            {
+                sortList.Read();
+            }
+            catch (MyExciption ex) { Console.WriteLine(ex.Message); }
+            void Sorted(int choice)
+            {
+                switch (choice)
+                {
+                    case 1:
+                        Family.Sort();
+                        foreach (string family in Family)
+                        {
+                            Console.WriteLine(family);
+                        }
+                        break;
+                    case 2:
+                        Family.Sort();
+                        Family.Reverse();
+                        foreach (string i in Family)
+                        {
+                            Console.WriteLine(i);
+                        }
+                        break;
                 }
             }
         }
